5198aef592852597fb8294b73844a62203b70d34
Igor Tarasuik <ihortarasyk@gmail.com>
2020-12-04 15:12:55 +0200

    Added LeaveType Repositories and Added Databales and FontAwaseome and CRUD

---
+++ VacationManagement/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace VacationManagement.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ VacationManagement/Repository/LeaveHistoryRepository.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using VacationManagement.Contracts;
+using VacationManagement.Data;
+
+namespace VacationManagement.Repository
+{
+    public class LeaveHistoryRepository : ILeaveHistoryRepository
+    {
+        private readonly ApplicationDbContext _db;
+
+        public LeaveHistoryRepository(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public bool Create(LeaveHistory entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Delete(LeaveHistory entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ICollection<LeaveHistory> FindAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public LeaveHistory FindByID(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Save()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Update(LeaveHistory entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
